491fa46fa2ccaa09741e7c71ee3bda1fb6b1c1fe
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-14 14:16:14 -0500

    Update and rename daily_totals_cards.sql to trello_cards_activity.sql

---
+++ snapshot/trello_cards_activity.sql
@@ -0,0 +1,24 @@
+{{% snapshot trello_cards_activity %}}
+
+  {{
+        config(
+          target_schema='snapshots',
+          strategy='timestamp',
+          unique_key='id',
+          updated_at ='dateLastActivity' 
+          invalid_hard_deletes =True,
+        )
+    }}
+
+
+
+select IdBoard, name, url, id, idList, dateLastActivity from {{source('trello_boards', 'trello_cards_tbl') }}
+
+
+{{% endsnapshot %}}   
+
+
+
+
+
+
