982ec0d6b37f0cbd8e751b48cd2f2c0403058eb9
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-14 12:40:27 -0500

    Add files via upload

---
+++ models/staging/stg_invited_board_users.sql
@@ -0,0 +1,15 @@
+{{% set name = " " %}
+
+
+with invited_users as (
+
+    select memberships as members, name as board_name from trello_boards.members_boards_tbl where name = {{ name }}
+
+   )
+
+
+select * from invited_users
+
+
+ 
+
