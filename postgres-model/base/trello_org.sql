46276de04daa6a11f9df6263d5fbb10fa6275eb0
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-18 03:36:35 -0500

    Create trello_org.sql

---
+++ postgres-model/base/trello_org.sql
@@ -0,0 +1,9 @@
+with t_org as (
+
+select displayName, id from trello_boards.trello_org
+
+)
+
+select * from t_org
+~                                                                                                                                                                       
+~                       
