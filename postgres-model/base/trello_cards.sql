20695c71eac5c48691a3688b95a5f5336c05134c
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-18 03:31:12 -0500

    Create trello_cards.sql

---
+++ postgres-model/base/trello_cards.sql
@@ -0,0 +1,8 @@
+with board_cards as  (
+
+select IdBoard, name, url, id, idList  from trello_cards_tbl
+
+)
+
+SELECT * FROM board_cards
+                                                                                                                                                                                                 
