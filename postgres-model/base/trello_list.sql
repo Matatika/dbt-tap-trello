978f7e1dd9dc86d3fc7bf66fbd87659e132a683d
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-18 03:33:13 -0500

    Create trello_list.sql

---
+++ postgres-model/base/trello_list.sql
@@ -0,0 +1,8 @@
+with trello_stage_names as  (
+
+select * from trello_boards.trello_list_tbl
+
+)
+
+select  name, idBoard, id from trello_stage_names
+                                              
