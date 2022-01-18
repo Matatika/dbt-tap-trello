da33012d39597f2d26b6f46369664507bf3e8599
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-18 04:01:51 -0500

    Update null_test_cards.sql

--- postgres-tests/null_test_cards.sql
+++ postgres-tests/null_test_cards.sql
@@ -1,1 +1,1 @@
-select name as card_name, idBoard as BoardID, idList as ListID from trello_boards.trello_cards where idBoard is null
+select name as card_name, idBoard as BoardID, idList as ListID from trello_cards where idBoard is null
