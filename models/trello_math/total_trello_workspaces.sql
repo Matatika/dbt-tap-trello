9776d6ba80c55b25b7c2fe7069d4ad0866b2532f
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-20 02:50:00 -0500

    Update total_trello_workspaces.sql

--- models/trello_math/total_trello_workspaces.sql
+++ models/trello_math/total_trello_workspaces.sql
@@ -1,1 +1,26 @@
-// file 
+
+{{ config(materialized='table') }}
+
+with trello_board as (  
+
+select name, idOrganization from trello-13.trello_data.trello_board
+
+),
+
+
+org as (
+
+   select displayName as workspace_name, id as org_id from trello-13.trello_data.trello_org
+
+ ),
+
+ workspace_agg as (
+
+SELECT  trello_board.name, trello_board.idOrganization, org.workspace_name, org.org_id
+FROM trello_board, org
+WHERE trello_board.idOrganization = org.org_id 
+
+) 
+
+select count(distinct workspace_name) as number_of_workspace from workspace_agg
+
