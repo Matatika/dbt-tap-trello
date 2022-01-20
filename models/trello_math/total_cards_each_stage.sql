8670488b64743b0f54616e02edd5a4b1e62ed7f6
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-20 04:43:44 -0500

    Create total_cards_each_stage.sql

---
+++ models/trello_math/total_cards_each_stage.sql
@@ -0,0 +1,32 @@
+with all_boards as (
+
+    select name as board_name, id as board_id  from trello-13.trello_data.trello_board
+),
+
+
+specific_board as (
+
+    select board_name, board_id from all_boards 
+
+ ),
+
+ cards as (
+
+select name as card_name, idBoard as  BoardID, idList as idList_card  from trello-13.trello_data.trello_cards
+),
+
+stage_board as (
+
+select name as stage_name, idBoard as Board_number, id as stage_id  from trello-13.trello_data.trello_list
+
+),
+
+
+daily_total as (
+
+select  stage_board.stage_name, stage_board.Board_number, stage_board.stage_id, cards.card_name, cards.idList_card
+from stage_board, cards
+where stage_board.stage_id = cards.idList_card
+)
+
+select count(card_name) as total_cards, stage_name from daily_total group by stage_name
