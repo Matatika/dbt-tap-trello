4512194a8181cc9e0bf64b22230a825a88e2be3e
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-01-25 09:14:51 -0500

    Delete assert_total_cards_tests.sql

--- tests/base/assert_total_cards_tests.sql
+++
@@ -1,6 +0,0 @@
-select
-    count(total_cards)
-from {{ref('total_cards_each_stage')}}
-where stage_name not null
-having not count(*) = (select sum(total_cards)
-                        from {{ref('total_cards_each_stage')}})
