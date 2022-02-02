4ada83f2ae43d1a5d255ff39bb93cbd83a13a852
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-02-02 00:49:47 -0500

    Update trello_lists.sql

--- models/base/trello_lists.sql
+++ models/base/trello_lists.sql
@@ -1,15 +1,17 @@
-with select_source as (  
-  
-      select * from {{ source('trello_test', 'lists') }}   
-  
-   ),  
-   
- 
- renamed_columns as (   
-   
-    select closed, id as id_list, idboard as id_board, "name", pos, softlimit as soft_limit, subscribed from select_source 
-   
-   )   
-   
-   
+with select_source as (
+
+      select * from {{ source('trello_data', 'lists') }}
+
+   ),
+
+
+ renamed_columns as (
+
+    select closed, id as id_list, id_board, "name", pos, soft_limit, subscribed from select_source
+
+   )
+
+
  select * from renamed_columns
+                                                                                                                                                                       
+                                                                                                                                                                                                  
