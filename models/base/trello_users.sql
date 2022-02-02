81b64c334406c686218e00ed186cb3fbdf8a46c7
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-02-02 00:51:53 -0500

    Update trello_users.sql

--- models/base/trello_users.sql
+++ models/base/trello_users.sql
@@ -1,15 +1,17 @@
-with select_source as (  
-  
-      select * from {{ source('trello_test', 'users') }} 
-  
-   ),  
-   
-   
- renamed_columns as (   
-   
-     select boardid as board_id, fullname as full_name, "id" as user_id, username as user_name from select_source
-   
-   )   
-   
-  select * from renamed_columns 
- 
+with select_source as (
+
+      select * from {{ source('trello_data', 'users') }}
+
+   ),
+
+
+ renamed as (
+
+
+  select board_id, full_name, "id", username from select_source
+
+   )
+
+  select * from renamed
+
+
