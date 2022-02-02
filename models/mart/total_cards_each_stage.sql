b7143323ffe09e5abaeb0bf70b087ae08111aa9d
Michael Aboagye <79202520+mike-aboagye@users.noreply.github.com>
2022-02-02 00:57:19 -0500

    Update total_cards_each_stage.sql

--- models/mart/total_cards_each_stage.sql
+++ models/mart/total_cards_each_stage.sql
@@ -1,43 +1,37 @@
+with trello_boards as (
 
-   
- with  board_table as (   
-    
-       select board_id, "name" as board_name from {{ ref('trello_boards') }}  
-       
-        ),
-    
-  stage_table as (  
-    
-       select id_list as list_id, id_board, "name" as stage_name from {{ ref('trello_lists') }} 
-    
-      ),  
-      
- card_table as (  
-   
-       select "id", url as card_url, id_board, id_list as idlist_card, "name" from {{ ref('trello_cards') }}
-   
-         ), 
-         
-  
-  total_cards as  (
-      
- select board_table.board_name, card_table.name, card_url, stage_table.stage_name
- from board_table, card_table, stage_table
-  where board_table.board_id = card_table.id_board 
+select "id" as board_id, "name" as board_name from {{ ref('trello_boards') }}
 
-  and card_table.idlist_card = stage_table.list_id 
-    
-    ),   
-    
-  
-  final as (  
-    
-  select * from total_cards
- 
-     )   
-     
-  select  board_name, count(distinct "name") as total_num_cards, stage_name from "final"
-  group by stage_name, board_name
+      ),
+
+
+ trello_lists as (
+
+   select id_list, id_board, "name" as stage_name from {{ ref('trello_lists') }}
+
+   ) ,
+
+
+ trello_cards as (
+
+     select id_list, "name" as card_name from {{ ref('trello_cards') }}
+
+   ),
+
+
+  final as (
+
+ select trello_boards.board_name, trello_lists.stage_name, trello_cards.card_name
+ from trello_boards, trello_lists, trello_cards
+ where trello_boards.board_id = trello_lists.id_board
+ and trello_cards.id_list = trello_lists.id_list
+   )
+
+ select board_name, stage_name, count(card_name) as num_of_cards from final
+ group by board_name, stage_name
+
+
+
       
   
  
