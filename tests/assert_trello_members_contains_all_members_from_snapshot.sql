07e73f80c96b667f444a10685967538a3de642a1
danielpdwalker <danielpdwalker@gmail.com>
2022-03-07 13:42:52 +0000

    Removed unneeded where is_deleted clause from test trello_members

--- tests/assert_trello_members_contains_all_members_from_snapshot.sql
+++ tests/assert_trello_members_contains_all_members_from_snapshot.sql
@@ -3,6 +3,5 @@
 select
     distinct(id_member)
 from {{ ref('trello_members') }}
-where is_deleted = False
-and id_member not in (select id_member
+where id_member not in (select id_member
                 from {{ ref('trello_members_snapshot') }})
